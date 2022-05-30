511178d490bb77f756529873efca4edf7eb4c2cd
kumatira <kumano@val.co.jp>
2022-05-30 17:54:29 +0900

    YouTube Data APIからvideoIdが重複して返ることがあるため、集めたvideoIdをユニークにする処理を追加

--- func/makeVideoDataByChannel/index.ts
+++ func/makeVideoDataByChannel/index.ts
@@ -1,14 +1,13 @@
 import { VideoWithBatchSummary } from '../../lib/models/videoWithBatchSummary';
 import { YoutubeDataAPI } from '../../lib/youtube';
 import { InfrastructureS3, InfrastructureDynamoDB } from '../../lib/aws-infra';
-import { isRunOnLocal } from '../../lib/util';
-import * as util from 'util'
+import { isRunOnLocal, uniq } from '../../lib/util';
 
 const lambdaHandler = async (): Promise<undefined> => {
     const targetChannels: {id: string, name: string}[] = JSON.parse(await InfrastructureS3.getTextFromS3('vget-api-channels', 'channels.json'));
     console.log('targetChannels', targetChannels);
     const targetChannelIds = targetChannels.map((i): string => i.id);
-    const targetVideoIds = (await Promise.all(targetChannelIds.map((id) => YoutubeDataAPI.getVideoIdsByChannelID(id)))).flat();
+    const targetVideoIds = uniq((await Promise.all(targetChannelIds.map((id) => YoutubeDataAPI.getVideoIdsByChannelID(id)))).flat());
     const targetVideosWithBatchSummary = await Promise.all(targetVideoIds.map((id) => VideoWithBatchSummary.init(id)));
     const insertRecords = targetVideosWithBatchSummary.map((v) => v.getInsertItems()).flat();
     console.log('insertRecords', insertRecords);
--- lib/util/index.ts
+++ lib/util/index.ts
@@ -1,3 +1,7 @@
 export const isRunOnLocal = (): boolean => {
     return process.env.RUN_ENV === undefined;
 };
+
+export const uniq = <T>(array: T[]): T[] => {
+    return [...new Set(array)];
+}
